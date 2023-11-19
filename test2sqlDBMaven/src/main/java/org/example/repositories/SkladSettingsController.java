e5b8d55dc2e477380d6d8fe84e77e062b9d9f347
Evgeny Yanchenko <evgeny.yanchenko@gmail.com>
2023-11-19 15:11:56 +0300

    reworked settsklady

--- test2sqlDBMaven/src/main/java/org/example/repositories/SkladSettingsController.java
+++ test2sqlDBMaven/src/main/java/org/example/repositories/SkladSettingsController.java
@@ -13,34 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SkladSettingsController {
     @Autowired
     private SkladRepository skladRepository;
-
-
-//
-//    @GetMapping ("/UpravlenPre/UpravlenieProizvodstvom/SettSklady")
-//    public String showSettingsSkladyPage (Model model)
-//    {
-//        return "/UpravlenPre/UpravlenieProizvodstvom/SettSklady";
-//    }
-
-//
-//        @GetMapping("/UpravlenPre/UpravlenieProizvodstvom/SettSklady")//заполняем таблицу java из БД и посылаем в HTML Dolgnosti
-//    public String fillingAllSkladyTable(Model model) {
-//        Iterable<Sklady> skladElement = skladRepository.findAll();
-//        model.addAttribute("sklady", skladElement);
-//
-//        return "/UpravlenPre/UpravlenieProizvodstvom/SettSklady";
-//    }
-
-
-//    @GetMapping("/UpravlenPre/UpravlenieProizvodstvom/SettSklady")
-//    public String fillSkladTable(Model model)
-//    {
-//
-//        return "redirect:/UpravlenPre/UpravlenieProizvodstvom/SettSklady";
-//    }
-
-
-    // получаем данные из html формы, кидаем их в объект  newSklad и сохраняем в БД
     @PostMapping("/UpravlenPre/UpravlenieProizvodstvom/SettSklady")
     public String addToDB(@RequestParam String nameSklada,
                           @RequestParam String adressSklada,
