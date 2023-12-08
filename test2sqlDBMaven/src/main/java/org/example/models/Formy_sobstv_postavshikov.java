170ccb0a885972051fcabedd3a1178e48d28a339
Evgeny Yanchenko <evgeny.yanchenko@gmail.com>
2023-12-08 20:48:23 +0300

    added 11 new models

---
+++ test2sqlDBMaven/src/main/java/org/example/models/Formy_sobstv_postavshikov.java
@@ -0,0 +1,10 @@
+package org.example.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Formy_sobstv_postavshikov {
+
+
+}
---
+++ test2sqlDBMaven/src/main/java/org/example/models/Gotov_tovary.java
@@ -0,0 +1,8 @@
+package org.example.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Gotov_tovary {
+}
