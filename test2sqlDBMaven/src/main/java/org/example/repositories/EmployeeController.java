6f8ea97f9b5de1faa3033198b51e4764c064b9cb
Evgeny Yanchenko <evgeny.yanchenko@gmail.com>
2023-12-19 16:35:02 +0300

    edited set & ADD controller page. remove remove button to the ADD page.

--- test2sqlDBMaven/src/main/java/org/example/repositories/EmployeeController.java
+++ test2sqlDBMaven/src/main/java/org/example/repositories/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,29 @@ import java.util.Date;
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepo;
+
+    @Autowired
+    DolgnostiRepository dolgnRepo;
+
+    // Sotrudniki settings show page
+    @GetMapping("/UpravlenPre/SotrudnikiSet")
+    public String showSotrudnikiSettingsPage(Model model) {
+        Iterable<Employee> element = employeeRepo.findAll();
+        model.addAttribute("employee", element);
+        return "/UpravlenPre/SotrudnikiSet";
+    }
+
+
+    @GetMapping("/UpravlenPre/Sotrudniki&PositionsADD")
+    public String showPositionsADD(Model model) {
+        System.out.println("redirect to Sotrudniki&PositionsADD");
+
+        //add dolgnosti to select elmnt
+        Iterable<Dolgnosti> dolgnosti = dolgnRepo.findAll();
+        model.addAttribute("dolgnosti", dolgnosti);
+
+        return "/UpravlenPre/Sotrudniki&PositionsADD";
+    }
 
     @PostMapping("/UpravlenPre/Sotrudniki&PositionsADD")
     public String addToDB(@RequestParam String lastNAme,
@@ -29,12 +53,33 @@ public class EmployeeController {
 
 //        if("add".equals(action))
 //        {
-            Employee newEmployee = new Employee(lastNAme,firstName,sourName,adressEmployee,phoneEmployee, dateOfBirth);
-            employeeRepo.save(newEmployee);
+        Employee newEmployee = new Employee(lastNAme, firstName, sourName, adressEmployee, phoneEmployee, dateOfBirth);
+        employeeRepo.save(newEmployee);
 //        }
         System.out.println("Into Create Empl!!!!!!");
         return "redirect:/UpravlenPre/SotrudnikiSet";
     }
+
+//    @PostMapping("/UpravlenPre/Sotrudniki&PositionsADD")
+//    public String addToDB(@RequestParam String nazvanie_Dolgnosti,
+//                          @RequestParam String kategoriya,
+//                          @RequestParam int oklad,
+//                          Model model) {
+//
+//        Dolgnosti newDolgnost = new Dolgnosti(nazvanie_Dolgnosti,kategoriya,oklad);
+//        dolgnRepo.save(newDolgnost);
+//
+//        return "redirect:/UpravlenPre/Sotrudniki&PositionsADD";
+//    }
+
+//      2 forms data are processing
+//    @PostMapping("/processForm")
+//    public ResponseEntity<String> processForm(@RequestParam("data1") String data1, @RequestParam("data2") String data2) {
+//        // Обработка данных формы
+//        // ...
+//
+//        return ResponseEntity.ok("Success");
+//    }
 
 
 }
--- test2sqlDBMaven/src/main/java/org/example/repositories/UpravleniePredpriyatiem.java
+++ test2sqlDBMaven/src/main/java/org/example/repositories/UpravleniePredpriyatiem.java
@@ -34,76 +34,45 @@ public class UpravleniePredpriyatiem {
     private DolgnostiRepository dolgnRepo;
 
     @GetMapping("/UpravlenPre/UpravleniePredpriyatiem")
-    public String showUpravleniePage (Model model)
-    {
+    public String showUpravleniePage(Model model) {
         return "/UpravlenPre/UpravleniePredpriyatiem";
     }
 
 
     // Settings Sklady
     @GetMapping("/UpravlenPre/UpravlenieProizvodstvom/SettSklady")
-    public String showSettSkladyPage(Model model)
-    {
+    public String showSettSkladyPage(Model model) {
         Iterable<Sklady> skladElement = skladRepository.findAll();
-        model.addAttribute("sklady",skladElement);
+        model.addAttribute("sklady", skladElement);
         return "/UpravlenPre/UpravlenieProizvodstvom/SettSklady";
     }
 
-
-
-
-
     // StoresSettings show page
     @GetMapping("/UpravlenPre/StoreSettings")
-    public String showSettStoresPage(Model model)
-    {
+    public String showSettStoresPage(Model model) {
         Iterable<Stores> someStore = storesRepository.findAll();
         model.addAttribute("stores", someStore);
         return "/UpravlenPre/StoreSettings";
     }
 
-
-    // Sotrudniki settings show page
-    @GetMapping("/UpravlenPre/SotrudnikiSet")
-    public String showSotrudnikiSettingsPage(Model model)
-    {
-        Iterable<Employee> element = employeeRepo.findAll();
-        model.addAttribute("employee", element);
-        return "/UpravlenPre/SotrudnikiSet";
-    }
-
-    @GetMapping("/UpravlenPre/Sotrudniki&PositionsADD")
-    public String showPositionsADD(Model model) {
-        System.out.println("redirect to Sotrudniki&PositionsADD");
-
-        //add dolgnosti to select elmnt
-        Iterable<Dolgnosti> dolgnosti = dolgnRepo.findAll();
-        model.addAttribute("dolgnosti", dolgnosti);
-
-        return "/UpravlenPre/Sotrudniki&PositionsADD";
-    }
-
-
-        //    Формы собственности Поставщиков
-        @GetMapping("/UpravlenPre/FormSobstvennostiPostavshikov")
+    //    Формы собственности Поставщиков
+    @GetMapping("/UpravlenPre/FormSobstvennostiPostavshikov")
     public String showFormSobstvennostiPostavshikov(Model model) {
-            Iterable<Formy_sobstv_postavshikov> element = formSobstvRepo.findAll();
-            model.addAttribute("formy_sobstv_postavshikov", element);
+        Iterable<Formy_sobstv_postavshikov> element = formSobstvRepo.findAll();
+        model.addAttribute("formy_sobstv_postavshikov", element);
         return "/UpravlenPre/FormSobstvennostiPostavshikov";
     }
-    @PostMapping("/UpravlenPre/FormSobstvennostiPostavshikov")
-    public String addFormSobstv(@RequestParam ("action") String action,
-                          @RequestParam Long id_form_sobstv,
-                          @RequestParam String nazvanie_form_sobstv,
-                          Model model) {
 
-        if("add".equals(action))
-        {
+    @PostMapping("/UpravlenPre/FormSobstvennostiPostavshikov")
+    public String addFormSobstv(@RequestParam("action") String action,
+                                @RequestParam Long id_form_sobstv,
+                                @RequestParam String nazvanie_form_sobstv,
+                                Model model) {
+
+        if ("add".equals(action)) {
             Formy_sobstv_postavshikov newForma = new Formy_sobstv_postavshikov(nazvanie_form_sobstv);
             formSobstvRepo.save(newForma);
-        }
-        else if ("remove".equals(action))
-        {
+        } else if ("remove".equals(action)) {
             Formy_sobstv_postavshikov formaForRemove = formSobstvRepo.findById(id_form_sobstv).orElseThrow();
             formSobstvRepo.delete(formaForRemove);
         }
